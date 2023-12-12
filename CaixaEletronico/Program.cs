07aaaa90fcb8bf76afd796648fb9d9583b254a83
Giancarlo Salomone <g.salomone@live.com>
2023-12-12 10:37:25 -0300

    Projeto iniciado e concluido

--- CaixaEletronico/Program.cs
+++ CaixaEletronico/Program.cs
@@ -1,13 +1,68 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 class Program
 {
     static void Main()
     {
-        int saldo = 1000;  
-        
+        string usuarioCadastrado = "usuario";
+        string salt = GerarSal(); 
+        string senhaCadastrada = HashSenha("senha123", salt); 
+        int saldo = 1000;
+
         Console.WriteLine("Bem-vindo ao Caixa Eletrônico!");
 
+        if (AutenticarUsuario(usuarioCadastrado, senhaCadastrada, salt))
+        {
+            
+            ExibirMenu(ref saldo);
+        }
+        else
+        {
+            Console.WriteLine("Usuário ou senha incorretos. Encerrando programa.");
+        }
+    }
+
+    static bool AutenticarUsuario(string usuarioCadastrado, string senhaCadastrada, string salt)
+    {
+        Console.Write("Digite o usuário: ");
+        string usuarioDigitado = Console.ReadLine();
+
+        Console.Write("Digite a senha: ");
+        string senhaDigitada = Console.ReadLine();
+
+        
+        string senhaDigitadaHash = HashSenha(senhaDigitada, salt);
+
+        
+        return usuarioDigitado == usuarioCadastrado && senhaDigitadaHash == senhaCadastrada;
+    }
+
+   
+    static string GerarSal()
+    {
+        byte[] randomBytes = new byte[32];
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(randomBytes);
+        }
+        return Convert.ToBase64String(randomBytes);
+    }
+
+  
+    static string HashSenha(string senha, string salt)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] senhaComSal = Encoding.UTF8.GetBytes(senha + salt);
+            byte[] hashBytes = sha256.ComputeHash(senhaComSal);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+
+    static void ExibirMenu(ref int saldo)
+    {
         while (true)
         {
             Console.WriteLine("\nEscolha uma opção:");
